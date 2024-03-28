e628f00c946b65afd18980447eee0c933303eefd
felipetakiguchi <felipe_ntakiguchi@hotmail.com>
2024-03-28 15:28:40 -0300

    Added input animation

--- autowatch/src/components/filter/filter.tsx
+++ autowatch/src/components/filter/filter.tsx
@@ -18,14 +18,14 @@ export default function Filter({ setFilter }: { setFilter: any }) {
     return (
         <div className={`input_box ${isFocused ? 'focused' : ''}`}>
             <div className="icon_box">
-                <Image src={searchIcon} className="search_icon" alt="search icon" width={30} />
+                <Image src={searchIcon} className={`search_icon ${isFocused ? 'focused_icon' : 'unfocused_icon'}`} alt="search icon" width={30} />
             </div>
             <input
                 placeholder="Pesquisar..."
                 onChange={(e) => setFilter(e.target.value)}
                 onFocus={handleFocus}
                 onBlur={handleBlur}
-                className="input"
+                className={`input ${isFocused ? 'focused_input' : 'unfocused_input'}`}
             />
         </div>
     );
