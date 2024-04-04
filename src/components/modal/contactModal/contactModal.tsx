5202b89152a3b8112a63753c63181176d1e058ed
felipetakiguchi <felipe_ntakiguchi@hotmail.com>
2024-04-04 14:59:17 -0300

    add whatsapp api send message

--- src/components/modal/contactModal/contactModal.tsx
+++ src/components/modal/contactModal/contactModal.tsx
@@ -1,6 +1,20 @@
 import "./styles.sass";
 
 export default function ContactModal() {
+    const mobileNumber = "+41 99221-3693";
+    const message = "message test";
+    
+    const onSubmit = () => {
+        // Regex expression to remove all characters which are NOT alphanumeric 
+        let number = mobileNumber.replace(/[^\w\s]/gi, "").replace(/ /g, "");
+        // Appending the phone number to the URL
+        let url = `https://web.whatsapp.com/send?phone=${number}`;
+        // Appending the message to the URL by encoding it
+        url += `&text=${encodeURI(message)}&app_absent=0`;
+        // Open our newly created URL in a new tab to send the message
+        window.open(url, "_blank");
+    };
+
     return (
         <div className="modal">
             <div className="modal_content">
@@ -10,6 +24,7 @@ export default function ContactModal() {
                 <section className="modal_content">
                     <p className="modal_text"><b>Email:</b> felipe_ntakiguchi@hotmail.com</p>
                     <p className="modal_text"><b>Telefone:</b> (41) 99221-3693</p>
+                    <button onClick={() => onSubmit()}>Enviar Mensagem</button>
                 </section>
             </div>
         </div>
